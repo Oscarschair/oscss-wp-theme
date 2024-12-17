a86d54615b71fbc50fc8d720affec97f95eef919
Oscarschair <oscarschair@example.com>
2024-12-17 16:48:36 +0900

    ss

--- header.php
+++ header.php
@@ -1,5 +1,10 @@
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php wp_title(); ?></title>
-    <?php wp_head(); ?>
-</head>
+<?php
+
+/**
+ * ナビのアクティブクラス出しわけ
+ */
+// $active = ' p-header__menu__item--active';
+// $kouboActive = is_page(['koubo', 'fdo-koubo', 'players-koubo', 'fdo-koubo-contents']) || is_singular('fdo-koubo') ? $active : '';
+// $learningActive = is_page(['learning', 'fdo', 'public', 'fdo-contents', 'public-contents', 'fdo-roadmap']) || is_singular('learning-contents') ? $active : '';
+// $kyuminActive = is_page(['kyumin', 'outline', 'features', 'details', 'numbers', 'symbol']) ? $active : '';
+// $faqActive = is_page(['faq', 'faq-search']) || is_singular('faq') ? $active : '';
