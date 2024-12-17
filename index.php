14f73a8f8a84de68ff0b040554b1f6a32b591ce4
Oscarschair <oscarschair@example.com>
2024-12-17 16:41:38 +0900

    header and footer

--- index.php
+++ index.php
@@ -1,35 +1,8 @@
-<!DOCTYPE html>
-<html lang="ja">
+<?php
+get_header();
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name'); ?></title>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-</head>
+<!-- index.php -->
 
-<body>
-    <header>
-        <h1><?php bloginfo('name'); ?></h1>
-        <p><?php bloginfo('description'); ?></p>
-    </header>
-    <main>
-        HELLO WORLD
-        <?php
-        if (have_posts()) {
-            while (have_posts()) {
-                the_post();
-                echo '<h2>' . get_the_title() . '</h2>';
-                the_content();
-            }
-        } else {
-            echo '<p>投稿がありません。</p>';
-        }
-        ?>
-    </main>
-    <footer>
-        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-    </footer>
-</body>
-
-</html>
+<?php
+get_footer();
