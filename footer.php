eb7c22a37b8c5f0274434193d8a3ba992e374b18
Oscarschair <oscarschair@example.com>
2024-12-17 23:00:54 +0900

    footer update

--- footer.php
+++ footer.php
@@ -1,133 +1,34 @@
     <footer class="l-footer">
         <div class="l-footer__wrapper">
-            <nav class="p-footer__nav-link">
-                <div class="p-footer__nav-link__outer">
-                    <div class="p-footer__nav-link__inner">
-                        <header>
-                            <h4>
-                                <a href="<?= home_url() ?>">総合トップ</a>
-                            </h4>
-                        </header>
-                    </div>
-                </div>
-                <div class="p-footer__nav-link__outer">
-                    <div class="p-footer__nav-link__inner">
-                        <header>
-                            <h4>
-                                <a href="<?= home_url('/koubo/') ?>">休眠預金活用事業に関する公募</a>
-                            </h4>
-                        </header>
-                        <ul class="p-footer__nav-link__list u-mb-24">
-                            <li>
-                                <a href="<?= home_url('/fdo-koubo/') ?>"><strong>資金分配団体・活動支援団体の公募</strong></a>
-                            </li>
-                            <li>
-                                <a href="<?= home_url('/fdo-koubo-contents/') ?>">資金分配団体・活動支援団体の公募一覧</a>
-                            </li>
-                        </ul>
-                        <ul class="p-footer__nav-link__list">
-                            <li>
-                                <a href="<?= home_url('/players-koubo/') ?>"><strong>実行団体・支援対象団体の公募</strong></a>
-                            </li>
-                            <li>
-                                <a href="https://players-koubolist.kyuplat.com" target="_blank">実行団体・支援対象団体の公募一覧</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="p-footer__nav-link__inner">
-                        <header>
-                            <h4>
-                                <a href="<?= home_url('/learning/') ?>">学習・研修</a>
-                            </h4>
-                        </header>
-                        <ul class="p-footer__nav-link__list">
-                            <li>
-                                <a href="<?= home_url('/learning/fdo/') ?>"><strong>資金分配団体向けの研修について</strong></a>
-                            </li>
-                            <li>
-                                <a href="<?= home_url('/learning/fdo-contents/') ?>">資金分配団体向けPO研修一覧</a>
-                            </li>
-                            <li>
-                                <a href="<?= home_url('/learning/fdo-roadmap/') ?>">「10の力」学習コンテンツ一覧</a>
-                            </li>
-                        </ul>
-                        <ul class="p-footer__nav-link__list">
-                            <li>
-                                <a href="<?= home_url('/learning/public/') ?>"><strong>一般の皆様へ 〜休眠預金活用について〜</strong></a>
-                            </li>
-                            <li>
-                                <a href="<?= home_url('/learning/public-contents/') ?>">一般の皆様向け 休眠預金活用関連コンテンツ一覧</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="p-footer__nav-link__inner p-footer__nav-link__inner--one-half">
-                        <header>
-                            <h4>
-                                <a href="<?= home_url('/kyumin/') ?>">休眠預金活用とは？</a>
-                            </h4>
-                        </header>
-                        <div class="p-footer__nav-link__inner__wrapper">
-                            <ul class="p-footer__nav-link__list">
-                                <li>
-                                    <a href="<?= home_url('/kyumin/details/') ?>">休眠預金活用事業とは</a>
-                                </li>
-                                <li>
-                                    <a href="<?= home_url('/kyumin/features/#impact') ?>">社会的インパクト評価</a>
-                                </li>
-                                <li>
-                                    <a href="<?= home_url('/kyumin/features/#support') ?>">非資金的支援（伴走支援等）</a>
-                                </li>
-                                <li>
-                                    <a href="<?= home_url('/kyumin/features/#compliance') ?>">ガバナンス・コンプライアンス</a>
-                                </li>
-                                <li>
-                                    <a href="<?= home_url('/kyumin/features/#disclosure') ?>">情報公開</a>
-                                </li>
-                            </ul>
-                            <ul class="p-footer__nav-link__list">
-                                <li>
-                                    <a href="<?= home_url('/kyumin/features/#collaboration') ?>">連携・協働</a>
-                                </li>
-                                <li>
-                                    <a href="<?= home_url('/kyumin/features/#others') ?>">その他</a>
-                                </li>
-                                <li>
-                                    <a href="<?= home_url('/about/outline/') ?>">制度（助成・出資）の枠組み</a>
-                                </li>
-                                <li>
-                                    <a href="<?= home_url('/about/numbers/') ?>">数字で見る休眠預金活用事業</a>
-                                </li>
-                                <li>
-                                    <a href="<?= home_url('/about/symbol/') ?>">シンボルマーク</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="p-footer__nav-link__outer">
-                    <div class="p-footer__nav-link__inner">
-                        <header>
-                            <h4>
-                                <a href="<?= home_url('/my-page/') ?>">マイページ</a>
-                            </h4>
-                        </header>
-                    </div>
-                </div>
-            </nav>
             <nav class="p-footer__nav-menu">
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/Oscarschair"
+                            aria-label="Github"><i class="fab fa-github-square" aria-hidden="true"
+                                title="Github"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/ChairOscar"
+                            aria-label="Twitter"><i class="fab fa-twitter" aria-hidden="true" title="Twitter"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.chatwork.com/osc6133"
+                            aria-label="Chatwork"><img src="/application/files/3216/5036/2875/chatwork_logo.png"
+                                width="27" height="27" style="top:-6px;left:-6px;position:relative;"></a>
+                    </li>
+                </ul>
                 <ul class="p-footer__nav-menu__main">
                     <li class="p-footer__nav-menu__main__item">
-                        <a href="<?= home_url('/faq/') ?>">よくある質問</a>
+                        <a href="https://stg.oscarchair.jp/profile/">プロフィール</a>
                     </li>
                     <li class="p-footer__nav-menu__main__item">
-                        <a href="<?= home_url('/news/') ?>">ニュース</a>
+                        <a href="https://stg.oscarchair.jp/blog/">オスカーの学習帳</a>
                     </li>
                     <li class="p-footer__nav-menu__main__item">
-                        <a href="https://janpia.form.kintoneapp.com/public/kyuplat-contactus"
-                            target="_blank">本サイトへのご要望等</a>
+                        <a href="https://stg.oscarchair.jp/contact/">お問い合わせ</a>
                     </li>
-                    <li class="p-footer__nav-menu__main__item p-footer__nav-menu__main__item--external-link">
-                        <a href="https://www.janpia.or.jp/" target="_blank">運営組織</a>
+                    <li class="p-footer__nav-menu__main__item">
+                        <a href="https://stg.oscarchair.jp/download/">ダウンロード</a>
                     </li>
                 </ul>
                 <ul class="p-footer__nav-menu__sub">
