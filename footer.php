6a869421113f4dd9719c660c15c6be811d33f497
Oscarschair <oscarschair@example.com>
2024-12-21 14:50:54 +0900

    sns svg

--- footer.php
+++ footer.php
@@ -15,47 +15,31 @@
                 <span class="footer__site-title--main">HIROFUMI KURUMA</span>
                 <span class="footer__site-title--sub">GA4コンサルタント</span>
             </div>
-        </div>
-        <div class="footer__right">
             <ul class="footer__sns-list">
                 <li class="footer__sns-item">
                     <a href="https://www.facebook.com" class="footer__sns-link" target="_blank"
                         rel="noopener noreferrer">
-                        <img src="<?= get_template_directory_uri() ?>/assets/icons/facebook.svg" alt="Facebook"
+                        <img src="<?= get_template_directory_uri() ?>/assets/svg/facebook.svg" alt="Facebook"
                             class="footer__sns-icon">
                     </a>
                 </li>
                 <li class="footer__sns-item">
                     <a href="https://www.twitter.com" class="footer__sns-link" target="_blank"
                         rel="noopener noreferrer">
-                        <img src="<?= get_template_directory_uri() ?>/assets/icons/twitter.svg" alt="Twitter"
+                        <img src="<?= get_template_directory_uri() ?>/assets/svg/twitterx.svg" alt="Twitter"
                             class="footer__sns-icon">
                     </a>
                 </li>
                 <li class="footer__sns-item">
                     <a href="https://www.linkedin.com" class="footer__sns-link" target="_blank"
                         rel="noopener noreferrer">
-                        <img src="<?= get_template_directory_uri() ?>/assets/icons/linkedin.svg" alt="LinkedIn"
+                        <img src="<?= get_template_directory_uri() ?>/assets/svg/linkedin.svg" alt="LinkedIn"
                             class="footer__sns-icon">
                     </a>
                 </li>
             </ul>
         </div>
-    </div>
-
-    <div class="footer__bottom">
-        <div class="footer__bottom-left">
-            <p class="footer__copyright">&copy; <?= date('Y') ?> HIROFUMI KURUMA. All Rights Reserved.</p>
-        </div>
-        <div class="footer__bottom-right">
-            <ul class="footer__links">
-                <li class="footer__link-item">
-                    <a href="<?= home_url('/terms') ?>" class="footer__link">利用規約</a>
-                </li>
-                <li class="footer__link-item">
-                    <a href="<?= home_url('/privacy-policy') ?>" class="footer__link">プライバシーポリシー</a>
-                </li>
-            </ul>
+        <div class="footer__right">
             <nav class="footer__menu">
                 <?php
                 wp_nav_menu(array(
@@ -67,6 +51,19 @@
                 ?>
             </nav>
         </div>
+    </div>
+
+    <div class="footer__bottom">
+        <ul class="footer__links">
+            <li class="footer__link-item">
+                <a href="<?= home_url('/terms') ?>" class="footer__link">利用規約</a>
+            </li>
+            <li class="footer__link-item">
+                <a href="<?= home_url('/privacy-policy') ?>" class="footer__link">プライバシーポリシー</a>
+            </li>
+        </ul>
+        <p class="footer__copyright">&copy; <?= date('Y') ?> HIROFUMI KURUMA. All Rights Reserved.</p>
+
     </div>
 
 </footer>
